08e356348586f615a8be80c010f5a5b99b9db5c1
Tiffany Boyer <tboyercode@gmail.com>
2022-04-25 18:36:00 -0500

    add locate rivers file

---
+++ static/js/locate-rivers.js
@@ -0,0 +1,68 @@
+// TODO: figure out a button to search after map moves.
+// for now use this toggle for testing so we're not sending queries every time we move the map
+let initMap = true; 
+
+//
+// geolocation
+//
+const geoOptions = {
+    enableHighAccuracy: false,
+    timeout: 5000,
+    maximumAge: Infinity
+};
+
+//TODO handle errors with geolocation
+function geoError(err){
+    console.log(err.code);
+    console.log(err.message);
+}
+
+function displayUserCoords(position) {
+    const user_latitude  = position.coords.latitude;
+    const user_longitude = position.coords.longitude;
+
+    //move the map to the user's location
+    map.flyTo({
+        center: [
+            user_longitude,
+            user_latitude
+        ],
+        essential: true // this animation is considered essential with respect to prefers-reduced-motion
+    });
+    map.on('moveend', () => {
+        const bounds = map.getBounds();
+        if(initMap === true){
+            getRivers(bounds);
+            initMap = false;
+        }
+    })
+};
+
+navigator.geolocation.getCurrentPosition(displayUserCoords, geoError, geoOptions);
+
+//
+// Get rivers from Flask
+//
+function getRivers(bounds) {
+    const maxLat = bounds._ne.lat;
+    const maxLng = bounds._ne.lng;
+    const minLat = bounds._sw.lat;
+    const minLng = bounds._sw.lng;
+
+    fetch(`/rivers?maxLat=${maxLat}&minLat=${minLat}&maxLng=${maxLng}&minLng=${minLng}`)
+    .then(response => response.json())
+    .then(responseData => {
+        displayRivers(responseData);
+    });
+};
+
+function displayRivers(rivers){
+    for (let river in rivers){        
+        new mapboxgl.Marker()
+        .setLngLat([rivers[river]['longitude'], rivers[river]['latitude']])
+        .setPopup(new mapboxgl.Popup(
+            {closeButton: false})
+            .setHTML(`<a href="/river-detail/${rivers[river]['usgs_id']}">${river}</a>`)) 
+        .addTo(map)
+    }
+}
